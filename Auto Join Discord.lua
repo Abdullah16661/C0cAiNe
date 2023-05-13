2acbaf0453676c4e4d99db1d4958eaa6e2cb5101
Abdullah16661 <133114998+Abdullah16661@users.noreply.github.com>
2023-05-13 06:39:17 +0300

    Update Auto Join Discord.lua

--- Auto Join Discord.lua
+++ Auto Join Discord.lua
@@ -1,15 +1,16 @@
-h = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
-h({
-    Url = 'http://127.0.0.1:6463/rpc?v=1',
-    Method = 'POST',
-    Headers = {
-        ['Content-Type'] = 'application/json',
-        Origin = 'https://discord.com'
-    },
-    Body = HttpService:JSONEncode({
-        cmd = 'INVITE_BROWSER',
-        nonce = HttpService:GenerateGUID(false),
-        args = {code = 'dYHag43eeU'}
-    })
+local requests = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or request
+requests({
+   Url = "http://127.0.0.1:6463/rpc?v=1",
+   Method = "POST",
+   Headers = {
+       ["Content-Type"] = "application/json",
+       ["Origin"] = "https://discord.com"
+   },
+   Body = game:GetService("HttpService"):JSONEncode({
+       cmd = "INVITE_BROWSER",
+       args = {
+           code = "RwSqWs3h"
+       },
+       nonce = game:GetService("HttpService"):GenerateGUID(false)
+   }),
 })
-
